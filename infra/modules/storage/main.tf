1b66f49d85e9e2967c8cf6badcc8b55374a74328
Jonathan Scholtes <jscholtes128@gmail.com>
2026-03-06 15:47:11 -0600

    storage_account_name

--- infra/modules/storage/main.tf
+++ infra/modules/storage/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_storage_container" "containers" {
   for_each = toset(var.storage_containers)
 
   name                  = each.key
-  storage_account_name  = azurerm_storage_account.main.name
+  storage_account_id    = azurerm_storage_account.main.id
   container_access_type = "private"
 }
 
